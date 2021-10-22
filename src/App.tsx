c3dbaf7443a2be37339b82e0f9ec36b1115f948e
ansunrisein <ansunrisein@gmail.com>
2021-10-22 16:25:25 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+/* eslint-disable */
 import React from 'react'
+import {useRedirect, useRoutes} from 'hookrouter'
+import {Catalog, Me, Search} from './features/book/pages'
+import {Login} from './features/user/pages'
+import {News} from './features/common/pages'
+import {Header} from './features/common/components'
 
-const App: React.FC = () => <></>
 
-export default App
+const routes = {
+  '/library': () => <Catalog />,
+  '/login': () => <Login />,
+  '/news': () => <News />,
+  '/search': () => <Search />,
+  '/me': () => <Me />,
+}
+
+export const App: React.FC = () => {
+  const routeResult = useRoutes(routes)
+  useRedirect('/', '/library')
+
+  return (
+    <React.StrictMode>
+      <Header />
+      {routeResult || <span>Not Found</span>}
+    </React.StrictMode>
+  )
+}
